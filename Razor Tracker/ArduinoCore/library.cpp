63e811835c2e2a51668442d8b98461f40fd6af4c
Dennis Partin <dpartin@sagent-group.com>
2018-04-03 12:33:43 -0500

    reated atmel stuido 7 solution and compiled with '#' will be ignored, and an empty message aborts the commit.  #

---
+++ Razor Tracker/ArduinoCore/library.cpp
@@ -0,0 +1,15 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 4/3/2018 12:23:30 PM
+ * Author : SG_L_2
+ */ 
+
+#include "sam.h"
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
